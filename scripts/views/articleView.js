81247adfd8630be2812b8180e8bab139acb261e2
Merkanto <miro_georgiev_2000@yahoo.com>
2016-08-09 15:39:25 +0300

    Modify scripts/views/articleView.js

---
+++ scripts/views/articleView.js
@@ -0,0 +1,43 @@
+class ArticleView {
+    constructor(mainContentSelector) {
+        this._mainContentSelector = mainContentSelector;
+    }
+
+    showCreatePostPage(data, isLoggedIn) {
+        let _that = this;
+        let templateUrl;
+        if(isLoggedIn){
+            templateUrl = "templates/form-user.html";
+        }
+        else{
+            templateUrl = "templates/form-guest.html";
+        }
+        $.get(templateUrl, function (template) {
+            let renderedWrapper = Mustache.render(template, null);
+            $(_that_wrapperSelector).html(renderedWrapper);
+
+            $.get('templates/create-article.html', function (template) {
+                var renderedContent = Mustache.render(template, null);
+                $(_that._mainContentSelector).html(renderedContent);
+
+                $('#author').val(data.fullname);
+
+                $('#create-new-article-request-button').on('click', function (ev) {
+                    let title = $('#title').val();
+                    let author = $('#author').val();
+                    let content = $('#content').val();
+                    let date = moment().format("MMMM Do YYYY");
+
+                    let data = {
+                        title:title,
+                        author:author,
+                        content:content,
+                        date:date
+                    };
+
+                    triggerEvent('createPost', data);
+                })
+            });
+        });
+    }
+}
