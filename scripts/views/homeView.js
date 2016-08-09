02acf0b634409cfe0b9af1baaf6060d6dc6574e8
Merkanto <miro_georgiev_2000@yahoo.com>
2016-08-09 12:05:15 +0300

    Add scripts/views/homeView.js

---
+++ scripts/views/homeView.js
@@ -0,0 +1,36 @@
+class HomeView {
+    constructor(mainContentSelector) {
+        this._selector = mainContentSelector;
+    }
+    showGuestPage(mainData) {
+        let _that = this;
+            $.get('templates/welcome-guest.html', function (template) {
+            let renderedWrapper = Mustache.render(template, null);
+            $(_that._selector).html(renderedWrapper);
+            $.get('templates/articles.html', function (template) {
+                let articles = {
+                    articleContent: mainData
+                };
+                let renderedArticles = Mustache.render(template, articles);
+                $('.articles').html(renderedArticles);
+            });
+        });
+    }
+
+
+
+    showUserPage(mainData) {
+        let _that = this;
+        $.get('templates/welcome-user.html', function (template) {
+            let renderedWrapper = Mustache.render(template, null);
+            $(_that._selector).html(renderedWrapper);
+            $.get('templates/articles.html', function (template) {
+                let articles = {
+                    articleContent: mainData
+                };
+                let renderedArticles = Mustache.render(template, articles);
+                $('.articles').html(renderedArticles);
+            });
+        });
+    }
+}
